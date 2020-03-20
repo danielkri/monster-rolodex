943a041e20f40486979b88d9490ecb6af32246ac
danielkri <dlkn87@gmail.com>
2020-03-21 00:03:50 +0200

    Updates

---
+++ precache-manifest.29ceb9571b6dbc32e0e3fb51a97f0f06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6d2ecf99f8a4799ca5dae89e60c51a4",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "cd2a0e707c5ae78ddecf",
+    "url": "/monster-rolodex/static/css/main.ec9046ba.chunk.css"
+  },
+  {
+    "revision": "0d35040140e440a26978",
+    "url": "/monster-rolodex/static/js/2.1313f230.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monster-rolodex/static/js/2.1313f230.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd2a0e707c5ae78ddecf",
+    "url": "/monster-rolodex/static/js/main.df3c56d4.chunk.js"
+  },
+  {
+    "revision": "90506cc279e62bb14608",
+    "url": "/monster-rolodex/static/js/runtime-main.59621b72.js"
+  }
+]);
